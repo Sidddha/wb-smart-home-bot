a1b0d54152fc4e3e87bb7d1c6b0cef171cce1c97
Sidddha <nikitaprudov@gmail.com>
2023-05-27 22:09:20 +0300

    add mqtt

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit the script if any command fails
+set -e
+
 # Define variables for the script
 SERVICE_NAME="wb-bot"
 WORKING_DIR="/mnt/data/wb-smart-home-bot"
@@ -32,41 +35,13 @@ sudo apt-get install \
 
 if ! command -v docker &> /dev/null
 then
-    # Run wb-release command and capture output
-    output=$(wb-release)
-
-    # Extract the version number from the output
-    version=$(echo "$output" | cut -d ' ' -f 2)
-
-    # Compare the version number to the minimum required version
-    if [[ $version == "wb-2304" || $version > "wb-2304" ]]; then
-      echo "wb release is newer then wb-2304. Performing additional actions..."
-      apt install -y iptables
-      update-alternatives --set iptables /usr/sbin/iptables-legacy
-      update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-    fi
-
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    mkdir /mnt/data/etc/docker && ln -s /mnt/data/etc/docker /etc/docker
-    mkdir /mnt/data/.docker
-    sudo cat << EOF > /etc/docker/daemon.json
-{
-  "data-root": "/mnt/data/.docker"
-}
-EOF
-
-    apt update && apt install docker-ce docker-ce-cli containerd.io docker-compose
-
-fi
-
-# Install Docker Compose (if not already installed)
-if ! command -v docker-compose &> /dev/null
-then
-    sudo curl -L "https://github.com/docker/compose/releases/v2.17.2/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+  echo "Running installation script..."
+  bash ./install_docker.sh
 fi
 
 # Run docker-compose
 cd $WORKING_DIR
 docker-compose -f $COMPOSE_FILE up -d
+
+# Exit with success status code
+exit 0
